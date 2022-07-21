3657aabca753eeec5c418e63ae4c7581bd7a62e0
Ladislav Žižka <iosdevcz@protonmail.com>
2022-07-21 15:10:19 +0200

    Initial Commit

---
+++ Jioujiapp/JioujiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  JioujiappApp.swift
+//  Jioujiapp
+//
+//  Created by Ladislav Žižka on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct JioujiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
